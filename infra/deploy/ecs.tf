3efd119739d98dea9a9ec5226b31012780cb6a53
Charlie McMullan <thomas.c.mcmullan@gmail.com>
2024-11-25 18:35:44 +0000

    ecr variable correction

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -5,17 +5,17 @@
 resource "aws_iam_policy" "task_execution_role_policy" {
   name        = "${local.prefix}-task-exec-role-policy"
   path        = "/"
-  description = "Allow ECS to retreive images and add to logs"
+  description = "Allow ECS to retrieve images and add to logs"
   policy      = file("./templates/ecs/task-execution-role-policy.json")
 }
 
-resource "aws_iam_role" "task_excecution_role" {
+resource "aws_iam_role" "task_execution_role" {
   name               = "${local.prefix}-task-execution-role"
   assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
 }
 
-resource "aws_iam_role_policy_attachment" "task_excecution_role" {
-  role       = aws_iam_role.task_excecution_role.name
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
 }
 
@@ -50,92 +50,93 @@ resource "aws_ecs_task_definition" "api" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = aws_iam_role.task_excecution_role.arn
+  execution_role_arn       = aws_iam_role.task_execution_role.arn
   task_role_arn            = aws_iam_role.app_task.arn
 
-  container_definitions = jsonencode([
-    {
-      name              = "api"
-      image             = var.ecr_app_image
-      essential         = true
-      memoryReservation = 256
-      user              = "django-user"
-      environment = [
-        {
-          name  = "DJANGO_SECRET_KEY"
-          value = var.django_secret_key
-        },
-        {
-          name  = "DB_HOST"
-          value = aws_db_instance.main.address
-        },
-        {
-          name  = "DB_NAME"
-          value = aws_db_instance.main.db_name
-        },
-        {
-          name  = "DB_USER"
-          value = aws_db_instance.main.username
-        },
-        {
-          name  = "DB_PASS"
-          value = aws_db_instance.main.password
-        },
-        {
-          name  = "ALLOWED_HOSTS"
-          value = "*"
+  container_definitions = jsonencode(
+    [
+      {
+        name              = "api"
+        image             = var.ecr_app_image
+        essential         = true
+        memoryReservation = 256
+        user              = "django-user"
+        environment = [
+          {
+            name  = "DJANGO_SECRET_KEY"
+            value = var.django_secret_key
+          },
+          {
+            name  = "DB_HOST"
+            value = aws_db_instance.main.address
+          },
+          {
+            name  = "DB_NAME"
+            value = aws_db_instance.main.db_name
+          },
+          {
+            name  = "DB_USER"
+            value = aws_db_instance.main.username
+          },
+          {
+            name  = "DB_PASS"
+            value = aws_db_instance.main.password
+          },
+          {
+            name  = "ALLOWED_HOSTS"
+            value = "*"
+          }
+        ]
+        mountPoints = [
+          {
+            readOnly      = false
+            containerPath = "/vol/web/static"
+            sourceVolume  = "static"
+          }
+        ]
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
+            awslogs-region        = data.aws_region.current.name
+            awslogs-stream-prefix = "api"
+          }
         }
-      ]
-      mountPoints = [
-        {
-          readOnly      = false
-          containerPath = "/vol/web/static"
-          sourceVolume  = "static"
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
-          awslogs-region        = data.aws_region.current.name
-          awslogs-stream-prefix = "api"
+      },
+      {
+        name              = "proxy"
+        image             = var.ecr_proxy_image
+        essential         = true
+        memoryReservation = 256
+        user              = "nginx"
+        portMappings = [
+          {
+            containerPort = 8000
+            hostPort      = 8000
+          }
+        ]
+        environment = [
+          {
+            name  = "APP_HOST"
+            value = "127.0.0.1"
+          }
+        ]
+        mountPoints = [
+          {
+            readOnly      = true
+            containerPath = "/vol/static"
+            sourceVolume  = "static"
+          }
+        ]
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
+            awslogs-region        = data.aws_region.current.name
+            awslogs-stream-prefix = "proxy"
+          }
         }
       }
-    },
-    {
-      name              = "proxy"
-      image             = var.ecr_proxy_image
-      essential         = true
-      memoryReservation = 256
-      user              = "nginx"
-      portMappings = [
-        {
-          containerPort = 8000
-          hostPort      = 8000
-        }
-      ]
-      environment = [
-        {
-          name  = "APP_HOST"
-          value = "127.0.0.1"
-        }
-      ]
-      mountPoints = [
-        {
-          readOnly      = true
-          containerPath = "/vol/static"
-          sourceVolume  = "static"
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
-          awslogs-region        = data.aws_region.current.name
-          awslogs-stream-prefix = "proxy"
-        }
-      }
-    }
   ])
 
   volume {
@@ -170,7 +171,7 @@ resource "aws_security_group" "ecs_service" {
     protocol  = "tcp"
     cidr_blocks = [
       aws_subnet.private_a.cidr_block,
-      aws_subnet.private_b.cidr_block
+      aws_subnet.private_b.cidr_block,
     ]
   }
 
@@ -194,7 +195,7 @@ resource "aws_ecs_service" "api" {
   desired_count          = 1
   launch_type            = "FARGATE"
   platform_version       = "1.4.0"
-  enable_execute_command = aws_iam_role.task_excecution_role
+  enable_execute_command = true
 
   network_configuration {
     assign_public_ip = true
